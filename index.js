957e95f34df28dbc292ba391df60be117cc29358
Kotokaze <5r1j6lvag@relay.firefox.com>
2020-12-26 16:21:08 +0900

    implement: seventeen module

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 'use strict';
 
+/**
+ * 17の倍数ならば真を返す
+ * @param {number} num
+ * @return {boolean}
+ */
+function isMultipleOfSeventeen(num) {
+	return (num%17 === 0);
+}
+
 module.exports = {
+	isMultipleOfSeventeen
 }
